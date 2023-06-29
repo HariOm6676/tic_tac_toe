b7c80d6bf541f90b529b0148686321c43bea4168
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-06-29 10:03:49 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,139 @@
 import 'package:flutter/material.dart';
-void main()=> runApp(MyApp());
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:HomePage(),
+      home: HomePage(),
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   // const HomePage({super.key});
-  
-
 
   @override
   State<HomePage> createState() => _HomePageState();
-
 }
 
 class _HomePageState extends State<HomePage> {
-  bool oTurn=true;
-  List<String> displayElement =['','','','','','','','',''];
-  int oScore=0;
-  int xScore=0;
-  int filledBoxes=0;
+  bool oTurn = true;
+  List<String> displayElement = ['', '', '', '', '', '', '', '', ''];
+  int oScore = 0;
+  int xScore = 0;
+  int filledBoxes = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo[900],
+      backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
-          Expanded(child: Container(child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Padding(padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Player X',
-                  style: TextStyle(fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-                ),
-                Text(xScore.toString(),
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),
-                ),
-                
-              ],
+          Expanded(
+            child: Container(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Player X',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            xScore.toString(),
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Player O',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.white)),
+                          Text(
+                            oScore.toString(),
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
             ),
-            ),
-            Padding(padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Player O',style: TextStyle(fontWeight:FontWeight.bold,fontSize: 20,color: Colors.white)),
-                Text(
-                  oScore.toString(),
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              ],
-            ),),
-            
-            ]
-            ),
-            ),
-            ),
-            Expanded(
+          ),
+          Expanded(
               flex: 4,
               child: GridView.builder(
-              itemCount: 9,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), itemBuilder:(BuildContext context,int index){
-              return GestureDetector(
-                onTap: (){
-                  _tapped(index);
-                },
-                child: Container(decoration: BoxDecoration(border: Border.all(color: Colors.white)),
-                child: Center(child: Text(displayElement[index],
-                style: TextStyle(color: Colors.white,fontSize: 35),),),),
-              );
-            }
-            )
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _tapped(index);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white)),
+                        child: Center(
+                          child: Text(
+                            displayElement[index],
+                            style: TextStyle(color: Colors.white, fontSize: 35),
+                          ),
+                        ),
+                      ),
+                    );
+                  })),
+          Expanded(
+              child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _clearScoreBoard,
+                  child: Text("Clear Score Board"),
+                )
+              ],
             ),
-            Expanded(child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: _clearScoreBoard, child: Text("Clear Score Board"),)
-                ],
-              ),
-            ))
+          ))
         ],
       ),
     );
   }
-  void _tapped (int index){
-  setState(() {
-    if (oTurn && displayElement[index]==''){
-      displayElement[index]='O';
-      filledBoxes++;
-    }
-    else if(!oTurn&& displayElement[index]==''){
-      displayElement[index]='X';
-            filledBoxes++;
-    }
-    oTurn= !oTurn;
-    _checkWinner();
-  });
-}
-void _checkWinner() {
-     
+
+  void _tapped(int index) {
+    setState(() {
+      if (oTurn && displayElement[index] == '') {
+        displayElement[index] = 'O';
+        filledBoxes++;
+      } else if (!oTurn && displayElement[index] == '') {
+        displayElement[index] = 'X';
+        filledBoxes++;
+      }
+      oTurn = !oTurn;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
     // Checking rows
     if (displayElement[0] == displayElement[1] &&
         displayElement[0] == displayElement[2] &&
@@ -128,7 +150,7 @@ void _checkWinner() {
         displayElement[6] != '') {
       _showWinDialog(displayElement[6]);
     }
- 
+
     // Checking Column
     if (displayElement[0] == displayElement[3] &&
         displayElement[0] == displayElement[6] &&
@@ -145,7 +167,7 @@ void _checkWinner() {
         displayElement[2] != '') {
       _showWinDialog(displayElement[2]);
     }
- 
+
     // Checking Diagonal
     if (displayElement[0] == displayElement[4] &&
         displayElement[0] == displayElement[8] &&
@@ -160,30 +182,32 @@ void _checkWinner() {
       _showDrawDialog();
     }
   }
-void _showWinDialog(String winner)
-{
-  showDialog(
-    barrierDismissible: false,
-    context: context, builder: (BuildContext context){
-            return AlertDialog(
-              title: Text("\" "+winner+" \" is Winner !!!"),
-              actions: [
-                TextButton(onPressed:(){
-                            _clearBoard();
-                  Navigator.of(context).pop();
-                }, child: Text("Play Again"),)
-              ],
-            );
-    }
-    );
-    if (winner =='O'){
-      oScore++;
 
-    }
-    else if(winner =='X'){
+  void _showWinDialog(String winner) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("\" " + winner + " \" is Winner !!!"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  _clearBoard();
+                  Navigator.of(context).pop();
+                },
+                child: Text("Play Again"),
+              )
+            ],
+          );
+        });
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
       xScore++;
     }
-}
+  }
+
   void _showDrawDialog() {
     showDialog(
         barrierDismissible: false,
@@ -203,22 +227,24 @@ void _showWinDialog(String winner)
           );
         });
   }
-  void _clearBoard(){
+
+  void _clearBoard() {
     setState(() {
-      for (int i=0;i<9;i++){
-        displayElement[i]='';
+      for (int i = 0; i < 9; i++) {
+        displayElement[i] = '';
       }
     });
-    filledBoxes=0;
+    filledBoxes = 0;
   }
-  void _clearScoreBoard(){
+
+  void _clearScoreBoard() {
     setState(() {
-      xScore=0;
-      oScore=0;
-      for(int i=0;i<9;i++){
-        displayElement[i]='';
+      xScore = 0;
+      oScore = 0;
+      for (int i = 0; i < 9; i++) {
+        displayElement[i] = '';
       }
     });
-    filledBoxes=0;
+    filledBoxes = 0;
   }
 }
